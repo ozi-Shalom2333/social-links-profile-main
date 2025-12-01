11445c37f40432941421cb1926cc55a6748ec412
Ozioma Okafor <okaozisol45@gmail.com>
2025-12-01 01:17:48 +0100

    fixed image path

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@
 const Intro = () => {
   return (
     <div className="w-[70px] h-[70px] rounded-full bg-amber-500 flex overflow-hidden items-center justify-center">
-      <img src="src/assets/images/avatar-jessica.jpeg" className="w-full h-full object-cover" alt="" />
+      <img src="/assets/images/avatar-jessica.jpeg" className="w-full h-full object-cover" alt="profile-picture" />
     </div>
   )
 }
